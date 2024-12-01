3f404b42d404d8e7b5aab066bc5e54779bd2df65
Mykola Zaiarnyi <mzaiarnyi@gmail.com>
2024-12-01 12:35:24 +0100

    chore: add instuction how to fill put the form

--- src/components/ui/instruction.tsx
+++ src/components/ui/instruction.tsx
@@ -1,3 +1,19 @@
-export function Instrukcja() {
-  return <div>Instrukcja Importu</div>;
+type InstructionsProps = {
+  title: string;
+  steps: string[];
+};
+
+export function Instruction({ title, steps }: InstructionsProps) {
+  return (
+    <div className="bg-gray-100 p-4 rounded-md shadow-md mb-6">
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <ul className="list-decimal list-inside text-gray-700">
+        {steps.map((step, index) => (
+          <li key={index} className="mb-1">
+            {step}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import { FormComponent } from "@/components/form";
-import { Instrukcja } from "@/components/ui/instruction";
+import { Instruction } from "@/components/ui/instruction";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,12 +19,19 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Instruction
+          title="Jak wypełnić formularz"
+          steps={[
+            "Wprowadź dane w odpowiednie pola, musisz uzupełnić wszystko.",
+            "Zaznacz przełacznik po Peselu jeśli potrzebujesz wprowadzić Paszport",
+            "Wybierz daty rozpoczęcia i zakończenia ubezpieczenia, nie musisz wprowadzać 2 razy, formularz to robi za ciebie",
+            "Jeśli potrzebujesz wprowadzić okres obowiazywania produktu osobno zaznacz przełącznik zaraz po wprowadzonym okresie ubezpieczenia",
+            "Sprawdź podgląd i kliknij 'Pobierz Plik', aby wygenerować dokument.",
+          ]}
+        />
+
         <FormComponent />
       </main>
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <Instrukcja />
-      </footer>
     </div>
   );
 }
